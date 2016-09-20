d143126f0ec26927c1858018b7bef322175286a6
Your Name <wiwjp619@gmail.com>
2016-09-20 05:45:20 +0100

    Fix the guidance's display

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ AUTH_BASE = 'http://localhost:2048/auth'
 # http://localhost:2048/auth/authorize is used for authorization page
 # http://localhost:2048/auth/token is used for exchange access taken
 # see details in auth.py
-CLIENT_ID = '86c10fcf-8827-4173-8157-decd8b1e259b'
+CLIENT_ID = 'c3425824-351f-4b54-b607-2ec0640dbecc'
 REDIRECT_URI = 'http://localhost:8000/recv_redirect'
 
 SCOPES = ['user/Sequence.read','user/Sequence.write',
