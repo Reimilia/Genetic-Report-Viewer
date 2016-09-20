546ba7962e3cec9cb5e94934b371ac3b3e146fb9
Wiwjp <wiwjp619@gmail.com>
2016-09-20 18:08:44 -0400

    Fixed the display

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ AUTH_BASE = 'http://localhost:2048/auth'
 # http://localhost:2048/auth/authorize is used for authorization page
 # http://localhost:2048/auth/token is used for exchange access taken
 # see details in auth.py
-CLIENT_ID = 'c3425824-351f-4b54-b607-2ec0640dbecc'
+CLIENT_ID = 'd416471a-9c66-4a31-b948-f599259337ac'
 REDIRECT_URI = 'http://localhost:8000/recv_redirect'
 
 SCOPES = ['user/Sequence.read','user/Sequence.write',
